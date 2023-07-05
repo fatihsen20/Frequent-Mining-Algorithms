6654e9fd7e07e8fcdf476357ddb7036ddacdf90d
fatihsen20 <fatih.sn2000@gmail.com>
2023-07-05 16:07:33 +0300

    fixed style.

--- src/FIM/fpgrowth.py
+++ src/FIM/fpgrowth.py
@@ -5,15 +5,14 @@ import itertools
 import numpy as np
 import pandas as pd
 
-from FIM.utils import (itemsets_transformation, 
-                        itemset_optimisation)
+from FIM.utils import itemsets_transformation
 
 
 def fpgrowth(df: pd.DataFrame,
-            min_support: float = 0.5,
-            show_colnames: bool = True,
-            max_len: int = None
-            ) -> pd.DataFrame:
+             min_support: float = 0.5,
+             show_colnames: bool = True,
+             max_len: int = None
+             ) -> pd.DataFrame:
 
     if min_support <= 0.0:
         raise ValueError(
@@ -21,7 +20,6 @@ def fpgrowth(df: pd.DataFrame,
             "number within the interval `(0, 1)`. "
             "Got %s." % min_support
         )
-    
     minsupp = math.ceil(min_support * len(df))
 
     fptree, _ = construct_fptree(df, minsupp)
@@ -49,22 +47,23 @@ def fpgrowth_driver(tree, minsupp, max_len):
                 count += 1
                 support = min([tree.nodes[i][0].count for i in transaction])
                 yield support, tree.conditional_items + list(transaction)
-    
+
     elif not max_len or len(tree.conditional_items) < max_len:
         for item in items:
             count += 1
             support = sum([node.count for node in tree.nodes[item]])
             yield support, tree.conditional_items + [item]
-    
+
     if not tree.is_path() and (not max_len or len(tree.conditional_items) < max_len):
         for item in items:
             conditional_tree = tree.conditional_tree(item, minsupp)
             for sup, iset in fpgrowth_driver(conditional_tree, minsupp, max_len):
                 yield sup, iset
 
+
 def construct_fptree(df, minsupp):
     num_of_transactions = df.shape[0]
-    
+
     itemsets, _ = itemsets_transformation(df)
 
     item_support = np.array(np.sum(itemsets, axis=0))
@@ -80,7 +79,7 @@ def construct_fptree(df, minsupp):
         transaction = [item for item in non_null if item in rank]
         transaction.sort(key=rank.get, reverse=True)
         fptree.add_transaction(transaction)
-    
+
     return fptree, rank
 
 
@@ -88,9 +87,9 @@ def generate_fis(generator, num_of_transaction):
     FREQUENTITEMSETS = {}
     for sup, items in generator:
         FREQUENTITEMSETS[frozenset(items)] = sup / num_of_transaction
-    
+
     res_df = pd.DataFrame([FREQUENTITEMSETS.values(), FREQUENTITEMSETS.keys()]).T
-    res_df.columns = ["support", "itemsets"] 
+    res_df.columns = ["support", "itemsets"]
     return res_df
 
 
@@ -100,7 +99,6 @@ class FPTree(object):
         self.nodes = collections.defaultdict(list)
         self.conditional_items = []
         self.rank = rank
-    
 
     def conditional_tree(self, conditional_item: str or int, minsupp: int):
         branches = []
@@ -124,10 +122,8 @@ class FPTree(object):
 
     def add_transaction(self, transaction, count=1):
         self.root.count += count
-
         if len(transaction) == 0:
             return
-        
         index = 0
         node = self.root
         for item in transaction:
@@ -138,20 +134,19 @@ class FPTree(object):
                 index += 1
             else:
                 break
-        for item in transaction[index: ]:
+        for item in transaction[index:]:
             child_node = FPNode(item, count, node)
             self.nodes[item].append(child_node)
             node = child_node
-    
 
     def is_path(self):
-        if len(self.root.children)>1:
+        if len(self.root.children) > 1:
             return False
         for i in self.nodes:
-            if len(self.nodes[i])>1 or len(self.nodes[i][0].children) > 1:
+            if len(self.nodes[i]) > 1 or len(self.nodes[i][0].children) > 1:
                 return False
         return True
-    
+
 
 class FPNode(object):
     def __init__(self, item, count=1, parent=None):
@@ -159,7 +154,6 @@ class FPNode(object):
         self.count = count
         self.parent = parent
         self.children = collections.defaultdict(FPNode)
-
 
     def itempath_from_root(self):
         path = []
@@ -171,4 +165,3 @@ class FPNode(object):
             node = node.parent
         path.reverse()
         return path
-
